c98fabbf368df3f188a3d959d0460ca811a6d2dc
carloswr0 <33141480+carloswr0@users.noreply.github.com>
2019-08-07 14:43:43 -0300

    Update search.js

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,12 +1,37 @@
-import { SEARCH_CHARACTERS, SEARCH_CHARACTERS_FAILURE } from '../actions/actionCreator';
+import { 
+  SEARCH_CHARACTERS, 
+  SEARCH_CHARACTERS_FAILURE, 
+  FETCH_MOVIE_FOR_SEARCHED_CHAR,
+  FETCH_MORE_SEARCH_CHARACTERS,
+  FETCH_MORE_SEARCH_CHARACTERS_FAILURE } from '../actions/actionCreator';
 
-const initialState = {};
+const initialState = { 
+  count: 0,
+  next: "",
+  previous: null,
+  results: []
+};
 
 function search(state = initialState, action) {
   switch(action.type) {
-
     case SEARCH_CHARACTERS:
     return  action.payload;
+
+    case FETCH_MOVIE_FOR_SEARCHED_CHAR:
+      const index = state.results.findIndex(e => e.name === action.payload.character);
+      const stateCopy = {...state};
+      stateCopy.results[index].movies = action.payload.moviesArray;  
+      return stateCopy;
+
+    case FETCH_MORE_SEARCH_CHARACTERS:
+      const stateCopy2 = {...state};
+      stateCopy2.next = action.payload.next;
+      const newResults = stateCopy2.results.concat(action.payload.results);
+      stateCopy2.results = newResults;
+      return stateCopy2;
+
+    case FETCH_MORE_SEARCH_CHARACTERS_FAILURE:
+      return state;
 
     case SEARCH_CHARACTERS_FAILURE:
       return state;
