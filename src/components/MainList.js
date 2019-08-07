0186fbf6c82a66c120632d3e8266959084d08109
carloswr0 <33141480+carloswr0@users.noreply.github.com>
2019-08-07 14:41:17 -0300

    Update MainList.js

--- src/components/MainList.js
+++ src/components/MainList.js
@@ -5,6 +5,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Search from './Search';
 import Loader from '../assets/loader';
+import Button from '@material-ui/core/Button';
 
 class MainList extends React.Component {
   componentDidMount() {
@@ -25,7 +26,8 @@ class MainList extends React.Component {
   };
   
   fetchData(section) {
-    if(this.props[this.props.location].length < 1) {
+    const {location} = this.props;
+    if(this.props[location].results.length < 1) {
       switch(section) {
         case 'characters':
           this.props.getAllCharacters();
@@ -42,7 +44,17 @@ class MainList extends React.Component {
   };
   
   render() {
-    const results = this.props[this.props.location];
+    const {location} = this.props;
+    let originArray = 'characters';
+    let results = this.props[location].results;
+   
+    if(this.props.search.results.length > 0 && this.props.filter.length > 0 && location === 'characters') {
+      results = this.props.search.results;
+      originArray = 'search';
+    } else {
+      results = this.props[location].results;
+    }
+
     let filteredArray = [];
 
     if(this.props.filter) {
@@ -56,9 +68,7 @@ class MainList extends React.Component {
     } else {
       filteredArray = results;
     };
-
    
-
     return (
       <div className="main-list">
         <Search {...this.props}></Search>
@@ -66,7 +76,7 @@ class MainList extends React.Component {
           {
             filteredArray.length > 0 ? filteredArray.map((e, key) => {
               return(
-                <List component="nav" aria-label="main mailbox folders" key={key} onClick={() => this.props.selectEntity(e, this.props.location)}>
+                <List component="nav" aria-label="main mailbox folders" key={key} onClick={() => this.props.selectEntity(e, this.props.location, originArray)}>
                   <ListItem button>
                     <ListItemText primary={e.title ? e.title : e.name} />
                   </ListItem>
@@ -75,6 +85,14 @@ class MainList extends React.Component {
             }) : this.props.filter && filteredArray.length < 1 ? <h1 className="not-found">No se encontraron coincidencias.</h1> : <Loader></Loader>
           } 
         </div>
+        {
+          location === 'characters' ?
+          <div className="load-more"> 
+              <Button variant="contained" onClick={() => this.props.getMoreCharacters(this.props[originArray].next, originArray)}>
+                Cargar más.
+              </Button>
+          </div> : null
+        }
       </div>
     );
   }
